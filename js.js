7ee1d512af2f8ecdbdc67a700de5cd4bb1962713
Xavimat <xaviermatoses@gmail.com>
2022-02-18 20:47:07 +0100

    use POST method

--- js.js
+++ js.js
@@ -8,19 +8,22 @@ function sendRequest() {
     resp.innerHTML += '<span class="spinner-grow spinner-grow-sm"></span> <span class="spinner-grow spinner-grow-sm"></span> <span class="spinner-grow spinner-grow-sm"></span>';
 
     if (req) {
-        req = 'http://localhost/xavi-mat.github.io/api.php?input=' + req;
+        req = 'http://localhost/xavi-mat.github.io/api.php';
         let reqbtn = document.querySelector('#reqbtn');
         reqbtn.disabled = true;
         reqbtn.innerHTML = '<span class="spinner-border spinner-border-sm"></span> Enviando...';
         fetch(req, {
-            method: 'GET',
+            method: 'POST',
             mode: 'cors',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify({input: req})
         })
             .then((resp)=>resp.json())
             .then((data)=>{
+                console.log(data);
                 resp.innerHTML = data.response;
                 reqbtn.innerHTML = 'Enviar';
                 reqbtn.disabled = false;
